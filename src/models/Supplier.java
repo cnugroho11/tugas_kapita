a8807e7eb32c0d65dcb79006f2c81ba4215391a3
cnugroho11 <cnugroho211@gmail.com>
2020-10-03 20:45:42 +0700

    implement mvc

---
+++ src/models/Supplier.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package models;
+
+import java.sql.Date;
+
+/**
+ *
+ * @author cnugr
+ */
+public class Supplier {
+    private String id;
+    private String nama;
+    private Date joinDate;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public Date getJoinDate() {
+        return joinDate;
+    }
+
+    public void setJoinDate(Date joinDate) {
+        this.joinDate = joinDate;
+    }
+    
+    
+}
---
+++ src/tools/Koneksi.java
@@ -0,0 +1,37 @@
+package tools;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author cnugr
+ */
+
+import com.mysql.jdbc.jdbc2.optional.MysqlDataSource;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+
+public class Koneksi {
+    public static Connection con;
+    public static Statement stm;
+    public static Connection getKoneksi() {
+        try{
+            //MysqlDataSource
+            String url="jdbc:mysql://localhost/tugas_kapita";
+            String user="root";
+            String pass="";
+            Class.forName("com.mysql.jdbc.Driver");
+            con=DriverManager.getConnection(url,user,pass);
+            stm=con.createStatement();
+            System.out.println("Koneksi mantap bos");
+        } catch(Exception e){
+            System.out.println("Koneksi gagal bos"+e.getMessage());
+        }
+        return con;
+    }
+}
