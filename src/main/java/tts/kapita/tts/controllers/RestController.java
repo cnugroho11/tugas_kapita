c00890e44e6d288a83f668413f030e8cfc331b2a
cnugroho11 <cnugroho211@gmail.com>
2020-11-17 23:49:23 +0700

    more templating

--- src/main/java/tts/kapita/tts/controllers/RestController.java
+++ src/main/java/tts/kapita/tts/controllers/RestController.java
@@ -10,6 +10,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import tts.kapita.tts.services.LoginRestService;
 import tts.kapita.tts.services.RegisterRestService;
 import tts.kapita.tts.entities.rest.LoginInput;
